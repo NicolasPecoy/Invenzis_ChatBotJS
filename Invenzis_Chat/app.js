5ad8d5931c71649aa04784e2c884624b9982ce83
Nicolas <nicolaspecoy@gmail.com>
2023-04-18 23:31:20 -0300

    control version ChatBotJS

---
+++ Invenzis_Chat/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = process.env.PORT || 5000;
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", function(socket){
+    socket.on("newuser", function (username) {
+      socket.broadcast.emit("update", username + " se unió a el chat.");
+    });
+    socket.on("exituser", function (username) {
+      socket.broadcast.emit("update", username + " se fue a el chat.");
+    });
+    socket.on("chat", function (username) {
+      socket.broadcast.emit("chat", message);
+    });
+});
+
+server.listen(port);
---
+++ Invenzis_Chat/public/code.js
@@ -0,0 +1,68 @@
+(function() {
+  const app = document.querySelector(".app");
+  const socket = lo();
+  let uname = "Usuario";
+  /*app
+    .querySelector(".join-screen #join-user")
+    .addEventListener("click", function() {
+      let username = app.querySelector(".join-screen #username").value;
+      if (username.length == 0) {
+        return;
+      }
+      socket.emit("newuser", username);
+      uname = username;
+      app.querySelector(".join-screen").classList.remove("active");
+      app.querySelector(".chat-screen").classList.add("active");
+    });*/
+
+  app
+    .querySelector(".chat-screen #send-message")
+    .addEventListener("click", function() {
+      let message = app.querySelector(".chat-screen #message-input").value;
+      if (message.length == 0) {
+        return;
+      }
+      renderMessage("my", {
+        username: uname,
+        text: message
+      });
+      socket.emit("chat", {
+        username: uname,
+        text: message
+      });
+      app.querySelector(".chat-screen #message-input").value = "";
+    });
+
+
+  function renderMessage(type, message) {
+    let messageContainer = app.querySelector(".chat-screen .messages");
+    if (type == "my") {
+      let el = document.createElement("div");
+      el.setAttribute("class", "message my-message");
+      el.innerHTML = (
+        <div>
+          <div class="name">You</div>
+          <div class="text">${message.text}</div>
+        </div>
+      );
+      messageContainer.appendChild(e1);
+    } else if (type == "other") {
+        let el = document.createElement("div");
+        el.setAttribute("class", "message other-message");
+        el.innerHTML = (
+          <div>
+            <div class="name">Invenzis</div>
+            <div class="text">${message.text}</div>
+          </div>
+        );
+        messageContainer.appendChild(e1);
+    } else if (type == "update") {
+        let el = document.createElement("div");
+        el.setAttribute("class", "update");
+        el.innerText = message;
+        messageContainer.appendChild(e1);
+    }
+    //scroll al final del chat
+    messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+  }
+})();
